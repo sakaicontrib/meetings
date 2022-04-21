af5bff66d40986747344b4e76bbe371add1798bd
ropemar <ropemar@gmail.com>
2022-04-21 12:40:00 +0200

    Initial upload of source code
    Remove node folder
    
    Initial source code
    
    Initial source code
    
    Initial source code
    
    Inital working modal
    
    Improve modal and add delete confirm dialog
    
    Added i18n endpoint
    
    Add i18n and fix modal position
    
    Initial source - Unit tests
    
    Initial source code - i18n
    
    Initial source - README
    
    Small fixes and improvements

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
-# meetings
-Sakai Meetings Tool repository
+# SAKAI - Online Meetings tool
+
+Tool for Sakai dedicated to the creation, management and use of virtual meeting rooms based on different online videoconferencing providers.
+
+#### Features
+
+- Create and schedule new meetings.
+- Control meeting access by site or group participant selection.
+- Add meetings to the Sakai Calendar.
+- Optional email notifications to participants.
+- Simple, fast and responsive interface.
+
+
+## Current supported providers
+- Microsoft Teams
+
+## Prerequisites
+You need:
+- A server running Sakai.
+- For Microsoft Teams integration:
+  - A Microsoft Azure Active Directory application.
+  - Azure Active Directory users must have the same email in Sakai to be identified as members of the organization.
+
+## MS Teams configuration
+You must create a new application in the  _App Registrations_ section of the Azure Active Directory portal. To grant **Meetings tool** access to yoor registered Azure application, you will need a **client secret**. To obtain this, you can access the _Certificates & secrets_ section within the configuration page of your registered Azure application.
+
+Once the app is created, you need to configure the permissions for your registered Azure App in the _API Permissions_ section. To add a new permission you must click _Add a permission_, then select _Microsoft Graph_ and _Application Permissions_. The permissions to enable are defined in the following table:
+
+```sh
+Directory.Read.All
+Directory.ReadWrite.All
+Group.Create
+Group.Read.All
+Group.ReadWrite.All
+OnlineMeetings.ReadWrite.All
+Team.Create
+Teamwork.Migrate.All
+User.Read
+User.Read.All
+```
+
+To finish, you must click on the _Grant admin consent_ button for your directory.
+
+## Sakai configuration
+You have to configure your server's Sakai properties file so that the Meetings tool can access your Azure App. These are the properties you need to configure:
+
+
+| PROPERTY | VALUE | DESCRIPTION | 
+| ------ | ------ | ------ |
+| meetings.msteams.authority | https://login.microsoftonline.com/{tenant}/ | {tenant} is the Tenant ID of your Azure Active Directory |
+| meetings.msteams.clientId | {clientId} | {clientId} is the Application (Client) Id from your _App registration_  |
+| meetings.msteams.secret | {secret} | This is the secret you created under _Certificates & secrets_ section  |
+| meetings.msteams.scope | https://graph.microsoft.com/.default | This is a fixed value |
---
+++ ui/src/main/frontend/src/views/Main.vue
@@ -0,0 +1,320 @@
+<template>
+  <div>
+    <div class="header-menu d-flex flex-column flex-md-row gap-2 div-heigth" :class="editPermission ? 'mb-4' : ''">
+      <div class="order-1 me-md-auto">
+        <SakaiButton
+          v-if="editPermission"
+          :text="i18n.create_new_meeting"
+          @click="handleCreateNewMeeting"
+          class="w-100"
+        >
+          <template #prepend>
+            <SakaiIcon class="me-1" iconkey="plus" />
+          </template>
+        </SakaiButton>
+      </div>
+      <SakaiInput
+        type="search"
+        :placeholder="i18n.search"
+        class="order-0 order-md-2 w-auto"
+        style="min-width: 20%"
+        v-model:value="searchString"
+      >
+      </SakaiInput>
+      <!-- 
+      <SakaiDropdownButton class="order-3" :items="items" text="Options">
+      </SakaiDropdownButton>
+       -->
+    </div>
+    <div v-if="searching && meetingsList.length > 0">
+      <div class="section-heading">
+        <h1 id="flush-headingOne" class="h4">{{ i18n.search_results }}</h1>
+        <hr aria-hidden="true" class="mb-0 mt-2" />
+      </div>
+      <div>
+        <div class="accordion-body p-0">
+          <div v-if="searchResult.length === 0" class="sak-banner-info">
+            {{ i18n.no_results }}
+          </div>
+          <ul
+            v-else
+            class="
+              list-unstyled
+              row row-cols-1 row-cols-md-2 row-cols-xl-3 row-cols-xxl-4
+              align-content-stretch
+            "
+          >
+            <li
+              class="col pt-4"
+              v-for="meeting in searchResult"
+              :key="meeting.id"
+            >
+              <SakaiMeetingCard
+                class="h-100"
+                :id="meeting.id"
+                :editable="editPermission"
+                :actions="meeting.actions"
+                @onDeleted="handleMeetingDelete"
+              >
+              </SakaiMeetingCard>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div v-if="meetingsList.length === 0" class="sak-banner-info">
+      {{ i18n.no_meetings_scheduled }}
+    </div>
+    <div v-if="happeningToday.length > 0 && !searching">
+      <div class="section-heading">
+        <h1 id="flush-headingOne" class="h4">{{ i18n.today }}</h1>
+        <hr aria-hidden="true" class="mb-0 mt-2" />
+      </div>
+      <div>
+        <div class="accordion-body p-0 pb-4">
+          <ul
+            class="
+              list-unstyled
+              row row-cols-1 row-cols-md-2 row-cols-xl-3 row-cols-xxl-4
+              align-content-stretch
+            "
+          >
+            <li
+              class="col pt-4"
+              v-for="meeting in happeningToday"
+              :key="meeting.id"
+            >
+              <SakaiMeetingCard
+                class="h-100"
+                :id="meeting.id"
+                :editable="editPermission"
+                :actions="meeting.actions"
+                @onDeleted="handleMeetingDelete"
+              >
+              </SakaiMeetingCard>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div v-if="inFuture.length > 0 && !searching">
+      <div class="section-heading">
+        <h1 class="accordion-header h4" id="flush-headingTwo">{{ i18n.future }}</h1>
+        <hr aria-hidden="true" class="mb-0 mt-2" />
+      </div>
+      <div>
+        <ul class="list-unstyled accordion-body p-0 pb-4">
+          <li class="row row-cols-1 row-cols-md-2 row-cols-xl-3 row-cols-xxl-4">
+            <div class="col pt-4" v-for="meeting in inFuture" :key="meeting.id">
+              <SakaiMeetingCard
+                class="h-100"
+                :id="meeting.id"
+                :editable="editPermission"
+                :actions="meeting.actions"
+                @onDeleted="handleMeetingDelete"
+              >
+              </SakaiMeetingCard>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div v-if="inPast.length > 0 && !searching">
+      <div
+        class="section-heading d-flex align-items-end"
+      >
+        <h1 class="mb-0 h4" id="flush-headingThree">{{ i18n.past }}</h1>
+<!--         
+        <div class="ms-auto">
+          <div @click="btnPress2 = !btnPress2" class="ms-auto">
+            <SakaiDropdownButton :items="showAll" text="Show All" :clear="true"></SakaiDropdownButton>
+          </div>
+        </div>
+-->
+      </div>
+      <hr aria-hidden="true" class="mb-0 mt-2" />
+      <div>
+        <div class="accordion-body p-0 pb-4">
+          <ul
+            class="
+              list-unstyled
+              row row-cols-1 row-cols-md-2 row-cols-xl-3 row-cols-xxl-4
+            "
+            v-if="inPast.length > 0"
+          >
+            <li class="col pt-4" v-for="meeting in inPast" :key="meeting.id">
+              <SakaiMeetingCard
+                class="h-100"
+                :id="meeting.id"
+                :editable="editPermission"
+                :actions="meeting.actions"
+                @onDeleted="handleMeetingDelete"
+              >
+              </SakaiMeetingCard>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import SakaiMeetingCard from "../components/sakai-meeting-card.vue";
+import SakaiInput from "../components/sakai-input.vue";
+import SakaiButton from "../components/sakai-button.vue";
+import SakaiDropdownButton from "../components/sakai-dropdown-button.vue";
+import SakaiIcon from "../components/sakai-icon.vue";
+import constants from "../resources/constants.js";
+import i18nMixn from "../mixins/i18n-mixn.js";
+import dayjs from "dayjs";
+import isTodayPlugin from "dayjs/plugin/isToday";
+dayjs.extend(isTodayPlugin)
+// eslint-disable-next-line
+
+export default {
+  name: "main",
+  components: {
+    SakaiMeetingCard,
+    SakaiInput,
+    SakaiButton,
+    SakaiDropdownButton,
+    SakaiIcon,
+  },
+  mixins: [i18nMixn],
+  data() {
+    return {
+      meetingsList: [],
+      searchString: '',
+      btnPress1: false,
+      btnPress2: false,
+      items: [
+        {
+          id: 0,
+          icon: "permissions",
+          string: "Permissions",
+          route: "/permissions",
+        },
+        {
+          id: 1,
+          icon: "template",
+          string: "Templates",
+          action: this.handleTemplates,
+        },
+      ],
+      showAll: [
+        {
+          id: 0,
+          icon: "all",
+          string: "All",
+          action: this.handleShowAll,
+        },
+        {
+          id: 1,
+          icon: "play",
+          string: "Recordings",
+          action: this.handleShowRecordings,
+        },
+      ]
+    };
+  },
+  methods: {
+    showError: function(message) {
+        this.$emit('showError', message);
+    },
+    handleCreateNewMeeting: function () {
+      this.$router.push({ name: "EditMeeting" });
+    },
+    handleTemplates: function () {
+    },
+    handleMeetingDelete: function (id) {
+      if (id) {
+        this.meetingsList = this.meetingsList.filter( (meeting) => meeting.id !== id );
+      }
+    },
+    handleMeetingEdit: function () {
+    },
+    handleShowAll: function () {
+    },
+    handleShowRecordings: function () {
+    },
+    loadMeetingsList: function () {
+      fetch(constants.toolPlacement + "/meetings/site/" + this.$route.params.siteid)
+      .then(r => {
+        if (r.ok) {
+          return r.json();
+        }
+        throw new Error(`Failed to get meetings`);
+      })
+      .then(data => {
+        data.forEach(meeting => {
+            meeting.live = false;
+            meeting.startDate = dayjs(meeting.startDate)
+              .tz(portal.user.timezone, true).format();
+            meeting.endDate = dayjs(meeting.endDate)
+              .tz(portal.user.timezone, true).format();
+            if (dayjs().isAfter(dayjs(meeting.startDate)) && dayjs().isBefore(dayjs(meeting.endDate))) {
+                meeting.live = true;
+            }
+        });
+        this.meetingsList = [...data];
+      })
+      .catch (error => this.showError(error));
+    },
+    meetingsComperator(a,b) {
+      return dayjs(a.startDate).isBefore(b.startDate) ? -1 : 1;
+    },
+    loadEditPermission() {
+      this.editPermission = false;
+      fetch(constants.toolPlacement + "/meetings/user/editperms/site/" + this.$route.params.siteid)
+      .then(r => {
+        if (r.ok) {
+          return r.json();
+        }
+        throw new Error(`Failed to get permissions`);
+      })
+      .then(data => {
+          this.editPermission = data;
+      })
+      .catch(error => this.showError(error));
+    },
+    switchtheme: function () {
+    },
+  },
+  computed: {
+    happeningToday: function () {
+      //Filter meetingsList for meetings that happen today, and are not over
+      return this.meetingsList.filter(
+        (meeting) =>
+          dayjs(meeting.startDate).isToday() && dayjs(meeting.endDate).isAfter(dayjs()) || meeting.live
+      ).sort(this.meetingsComperator);
+    },
+    inPast: function () {
+      return this.meetingsList.filter(
+        (meeting) =>
+          dayjs(meeting.startDate).isBefore(dayjs()) && !meeting.live
+      ).sort(this.meetingsComperator).reverse();
+    },
+    inFuture: function () {
+      return this.meetingsList.filter(
+        (meeting) =>
+          dayjs().isBefore(dayjs(meeting.startDate), "day") && !meeting.live
+      ).sort(this.meetingsComperator);
+    },
+    searching: function () {
+      return this.searchString !== '';
+    },
+    searchResult: function () {
+      let searchString = this.searchString.toLowerCase();
+      if (!this.searching) { return [] }
+      return this.meetingsList.filter(
+        (meeting) =>
+          meeting.title.toLowerCase().search(searchString) > -1 || meeting.description.toLowerCase().search(searchString) > -1
+      );
+    },
+  },
+  mounted: function () {
+    this.loadEditPermission();
+    this.loadMeetingsList();
+  },
+};
+</script>
